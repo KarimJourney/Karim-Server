b7f0fadda2f76e7585037cf2e3b63158ef6f23e2
hgyuhyeon <hwgyuhyeon@gmail.com>
2024-11-26 10:05:14 +0900

    [CHORE] cors 설정 변경

--- karim/src/main/java/com/karim/karim/config/WebConfig.java
+++ karim/src/main/java/com/karim/karim/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:5000") // 프론트엔드 출처
+                .allowedOriginPatterns("*") // 프론트엔드 출처
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true) // 자격 증명(쿠키, 인증 헤더 등) 허용
